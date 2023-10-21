e8d700aebeaeb365aa4a6a9ab0c8a08357fdd8d9
maxim <bozzhik@ya.ru>
2023-10-21 18:11:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Astro — template `astro` + `tailwind css`
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/bozzhik/astro_template.git
+```
+
+2. Go to the project folder
+
+```bash
+cd astro_template
+```
+
+3. Install dependencies
+
+```bash
+npm i
+```
+
+3. Start the development server
+
+```bash
+npm run dev
+```
+
+\*4. Or run the git command chain
+
+```bash
+git clone https://github.com/bozzhik/astro_template.git && cd astro_template && npm i && code .
+```
---
+++ tailwind.config.mjs
@@ -0,0 +1,40 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        grotesque: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'Roboto', 'sans-serif'],
+        monospaced: ['DM Mono'],
+      },
+      colors: {
+        gray: {
+          100: '#f0f0f0',
+          200: '#e4e4e4',
+          300: '#d1d1d1',
+          400: '#b4b4b4',
+          500: '#919191',
+          600: '#818181',
+          700: '#6a6a6a',
+          800: '#5a5a5a',
+          900: '#4e4e4e',
+        },
+      },
+    },
+    screens: {
+      xl: {max: '1536px'},
+      lg: {max: '1024px'},
+      sm: {max: '768px'},
+      xs: {max: '350px'},
+    },
+  },
+  plugins: [
+    // adds a `s-*` utility to apply the same width and height
+    plugin(function sizePlugin(api) {
+      api.matchUtilities({s: (value) => ({width: value, height: value})}, {values: api.theme('width')})
+    }),
+  ],
+}
